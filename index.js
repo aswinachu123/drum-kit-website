2ea726a61faa2568c3d79c1a1b6b6541174705a0
aswinachu123 <111508171+aswinachu123@users.noreply.github.com>
2023-01-27 19:10:14 +0530

    drum kit

---
+++ index.js
@@ -0,0 +1,75 @@
+//decting keyboard press
+
+
+var numberofDrumbuttons = document.querySelectorAll(".drum").length;
+
+for(var i = 0 ; i<numberofDrumbuttons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        var buttonInnerhtml = this.innerHTML;
+        makeSound(buttonInnerhtml);
+
+    });
+
+
+
+}
+//decting key board press
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    ;
+
+
+})
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        
+        case "j":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        
+        case "k":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();                    
+            break;
+        
+        case "l":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+                                
+        
+    
+        default :console.log(buttonInnerhtml)
+            break;
+    }
+}
+
+
+
+
+
+
+
